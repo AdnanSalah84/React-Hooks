2ad63c437924a7319e505eb7df2829ec9d11dcbc
Adnan Salahuddin <adnan.salah84@gmail.com>
2019-07-24 16:55:37 +0500

    Initial react hooks set state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [inputText, setInputText] = useState('');
+  const [historyList, setHistoryList] = useState([]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <input 
+          onChange={(e)=>{ 
+            setInputText(e.target.value);
+            setHistoryList([...historyList,e.target.value]);
+          }} 
+          placeholder="Enter some text"/>
+        <br/>
+        {inputText}
+        <hr/> <br/>
+        <ul>
+          {historyList.map((rec)=>{
+            return <div>{rec}</div>
+          })}
+        </ul>
     </div>
   );
 }
