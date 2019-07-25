76fd8dbda6ff55907350baa2ab22cc8b9340f2b0
Adnan Salahuddin <adnan_salah84@outlook.com>
2019-07-25 11:21:51 +0500

    Add useRef to enable mouseover and mouseon

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React, {useState} from 'react';
 import './App.css';
+import ImageToggleOnMouseOver  from './ImageToggleOnMouseOver';
+
 
 function App() {
+  return (
+    <div className="App">
+      {/* <InputElement />   */}
+      <ImageChangeOnMouseOver />
+    </div>
+  );
+}
 
+function InputElement(){
   const [inputText, setInputText] = useState('');
   const [historyList, setHistoryList] = useState([]);
 
   return (
-    <div className="App">
-
+    <div>
         <input 
           onChange={(e)=>{ 
             setInputText(e.target.value);
@@ -22,8 +31,21 @@ function App() {
           {historyList.map((rec)=>{
             return <div>{rec}</div>
           })}
-        </ul>
+        </ul> 
     </div>
+  );
+}
+
+function ImageChangeOnMouseOver(){
+  return (
+    <div>
+      {/* <img src="./speakers/bw/Speaker-187.jpg" alt=""/>
+      <img src="./speakers/bw/Speaker-1124.jpg" alt=""/> */}
+      <ImageToggleOnMouseOver primaryImg="./speakers/bw/Speaker-187.jpg" 
+                              secondaryImg="./speakers/Speaker-187.jpg" alt=""/>
+      <ImageToggleOnMouseOver primaryImg="./speakers/bw/Speaker-1124.jpg"
+                              secondaryImg="./speakers/Speaker-1124.jpg" alt=""/>
+   </div>
   );
 }
 
